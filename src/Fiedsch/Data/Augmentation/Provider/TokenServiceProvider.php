3ebcfe060ee0d1c16c2444ecd6e781b58739320b
fiedsch <fiedsch@ja-eh.at>
2016-01-23 11:15:39 +0100

    refactoring continued and new class for token generation

--- src/Fiedsch/Data/Augmentation/Provider/TokenServiceProvider.php
+++ src/Fiedsch/Data/Augmentation/Provider/TokenServiceProvider.php
@@ -12,111 +12,27 @@
 namespace Fiedsch\Data\Augmentation\Provider;
 
 use Pimple\Container;
-use  Pimple\ServiceProviderInterface;
+use Pimple\ServiceProviderInterface;
+use Fiedsch\Data\Utility\TokenCreator;
 
 class TokenServiceProvider implements ServiceProviderInterface {
-
-    protected $container;
-
-    protected $issued_tokens;
-
-    public function __construct() {
-
-        $this->issued_tokens = array();
-    }
 
     /**
      * @param Container $pimple the dependency injection container.
      */
     public function register(Container $container) {
 
-        $this->container = $container;
-
-        if (!$this->container->offsetExists('token.length')) {
-            $this->container['token.length'] = 10;
+        if (!$container->offsetExists('token.length')) {
+            $container['token.length'] = TokenCreator::DEFAULT_LENGTH;
         }
-        if (!$this->container->offsetExists('token.case')) {
-            $this->container['token.case'] = 'uc';
+        if (!$container->offsetExists('token.case')) {
+            $container['token.case'] = TokenCreator::UPPER;
         }
 
-        $this->container['token'] = $container->protect(function () {
-            return $this->getUniqueToken();
-        });
+        $container['token'] = function ($container) {
+            return new TokenCreator($container['token.length'], $container['token.case']);
+        };
 
-
-    }
-
-    /**
-     * Generate a unique token.
-     *
-     * @return string creates a new token that has not been issued before
-     */
-    protected function getUniqueToken() {
-        $tries = 0;
-        $candidate = $this->cretateToken();
-        // give up and throw an exception if it seems impossible to create a unique token
-        while ($tries++ < 5 && in_array($candidate, $this->issued_tokens)) {
-            $candidate = $this->cretateToken();
-        }
-        if (in_array($candidate, $this->issued_tokens)) {
-            throw new \RuntimeException("failed to create unique token");
-        }
-
-        $this->issued_tokens[] = $candidate;
-
-        return $candidate;
-    }
-
-    /**
-     * Generate a token
-     *
-     * @return string a Token
-     */
-    protected function cretateToken() {
-
-        // characters we want to omit in generated tokens as they can be confused
-        // if someone has to type the token
-
-        $bad_characters = array(
-            'i' => 'a', // might be confused with 1
-            'l' => 'b', // see 'i'
-            'o' => 'c', // might be confused with 0 (zero)
-            '1' => 'd', // see 'i'
-            '0' => 'f', // see 'o'
-            'e' => 'g', // if we use the results in Excel it might try to convert 'e123' to a number :-(
-        );
-
-        $token = sha1(rand());
-
-        $token_length = $this->container['token.length'];
-        while (strlen($token) < $token_length) {
-            $token .= sha1(rand());
-        }
-
-        foreach ($bad_characters as $bad => $replacement) {
-            $token = preg_replace("/$bad/", $replacement, $token);
-        }
-
-        // shorten to token.length
-
-        $token = substr($token, 0, $this->container['token.length']);
-
-        // convert to token.case
-        $case = strtolower($this->container['token.case']);
-        if (!in_array($case, array('uc', 'lc'))) {
-            throw new \RuntimeException("invalid case specificationn '$case'");
-        }
-
-        switch ($case) {
-            case 'lc':
-                $token = strtolower($token);
-                break;
-            case 'uc':
-                $token = strtoupper($token);
-                break;
-        }
-
-        return $token;
     }
 
 }
---
+++ src/Fiedsch/Data/Utility/TokenCreator.php
@@ -0,0 +1,118 @@
+<?php
+/**
+ * @package    Datamanagement
+ * @author     Andreas Fieger <fiedsch@ja-eh.at>
+ * @copyright  2016 Andreas Fieger
+ * @license    MIT
+ * @version    0.0.1
+ * @link       https://github.com/fiedsch/datamanagement
+ */
+
+namespace Fiedsch\Data\Utility;
+
+
+class TokenCreator {
+
+    const LOWER = 1;
+    const UPPER = 2;
+    const MIXED = 3;
+
+    const DEFAULT_LENGTH = 12;
+
+    protected $length;
+
+    protected $case;
+
+    protected $generated;
+
+    public function __construct($length = 10, $case = self::UPPER) {
+        if (!is_int($length) || $length < 1) {
+            throw new \RuntimeException("token length must be an integer");
+        }
+        if (!in_array($case, [self::LOWER, self::UPPER, self::MIXED])) {
+            throw new \RuntimeException(sprintf("token case must be one of %s=LOWER, %s=UPPER or %s=MIXED",
+                self::LOWER, self::UPPER, self::MIXED));
+        }
+        $this->length = $length;
+        $this->case = $case;
+        $this->generated = [ ];
+    }
+
+    /**
+     * Generate a unique token (meaning: this instance has never created and issued
+     * this token before).
+     *
+     * @return string the newly created unique token
+     */
+    public function getUniqueToken() {
+        $tries = 0;
+        $candidate = $this->cretateToken();
+        // give up and throw an exception if it seems impossible to create a unique token
+        while ($tries++ < 5 && in_array($candidate, $this->generated)) {
+            $candidate = $this->cretateToken();
+        }
+        if (in_array($candidate, $this->generated)) {
+            throw new \RuntimeException("failed to create a new unique token");
+        }
+
+        $this->generated[] = $candidate;
+
+        return $candidate;
+    }
+
+    /**
+     * Generate a token according to the rules (length and case)
+     *
+     * @return string a Token
+     */
+    protected function cretateToken() {
+
+        // characters we want to omit in generated tokens as they can be confused
+        // if someone has to type the token
+
+        $bad_characters = array(
+            'i' => 'a', // might be confused with 1
+            'l' => '2', // see 'i'
+            'o' => 'b', // might be confused with 0 (zero)
+            '1' => '3', // see 'i'
+            '0' => 'd', // see 'o'
+            'e' => '4', // if we use the results in Excel it might try to convert 'e123' to a number :-(
+        );
+
+        $token = '';
+        while (strlen($token) < $this->length) {
+            $token .= sha1(rand());
+        }
+
+        // replace characters that might be confusing
+
+        $token = str_replace(array_keys($bad_characters), array_values($bad_characters), $token);
+
+        // shorten to length
+
+        $token = substr($token, 0, $this->length);
+
+        // convert to case
+
+        switch ($this->case) {
+            case self::LOWER:
+                $token = strtolower($token);
+                break;
+            case self::UPPER:
+                $token = strtoupper($token);
+                break;
+            case self::MIXED:
+                // Leave as is would be all lowercase. So transform ~ 50% of the
+                // letters to uppercase
+                $token = join('', array_map(function($letter) {
+                    if (ctype_alpha($letter) && rand(0,1) > 0.5) {
+                        return strtoupper($letter);
+                    }
+                    return $letter;
+                }, str_split($token)));
+        }
+
+        return $token;
+    }
+
+}

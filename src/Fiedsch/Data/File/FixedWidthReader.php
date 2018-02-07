128b21b43d0a4538dbece3397ad613481bc60c1e
Andreas Fieger <fiedsch@ja-eh.at>
2018-02-07 07:39:31 +0100

    add FixedWidthReader

---
+++ src/Fiedsch/Data/File/FixedWidthReader.php
@@ -0,0 +1,120 @@
+<?php
+/**
+ * @package    Datamanagement
+ * @author     Andreas Fieger <fiedsch@ja-eh.at>
+ * @copyright  2016 Andreas Fieger
+ * @license    MIT
+ * @link       https://github.com/fiedsch/datamanagement
+ */
+
+namespace Fiedsch\Data\File;
+
+/**
+ * Class FixedWidthReader
+ * @package Fiedsch\Data
+ *
+ * Read a file line by line and return an array of data per line
+ * that has been created by splitting the line into fixed width fields.
+ *
+ * Files are assumed to be encoded utf-8 with LF line endings.
+ * @see FileReader for more information.
+ */
+
+class FixedWidthReader extends Reader
+{
+
+    /**
+     * @var array the positions (start and end) of the fields.
+     */
+    protected $fields;
+
+    /**
+     * Constructor.
+     *
+     * @param string $filepath a relative or absolute path to the file.
+     *
+     * @param array $fields the positions (start and end) of the fields to be read from each line of the file.
+     */
+    public function __construct($filepath, array $fields)
+    {
+        parent::__construct($filepath);
+        $this->fields = $fields;
+        $this->checkFieldsDefinition();
+    }
+
+    /**
+     * Check the fields definition array and throw an exception if it does not make sense.
+     * e.g. if we encounter negative values for 'from' or 'to' or if they are missing.
+     *
+     * @throws \RuntimeException
+     */
+    protected function checkFieldsDefinition()
+    {
+        if (count($this->fields) === 0) {
+            throw new \RuntimeException("fields definition is empty");
+        }
+        foreach ($this->fields as $i => $field) {
+            if (!isset($field['from'])) {
+                throw new \RuntimeException("'from' is missing for field '$i'");
+            }
+            if (!isset($field['to'])) {
+                throw new \RuntimeException("'to' is missing for field '$i'");
+            }
+            if (!is_integer($field['from'])) {
+                throw new \RuntimeException("'from' is not an integer (field '$i')");
+            }
+            if (!is_integer($field['to'])) {
+                throw new \RuntimeException("'to' is not an integer (field '$i')");
+            }
+            if ($field['from']<0) {
+                throw new \RuntimeException("'from' is negative (field '$i')");
+            }
+            if ($field['to']<0) {
+                throw new \RuntimeException("'to' is negative (field '$i')");
+            }
+            if ($field['from']>$field['to']) {
+                throw new \RuntimeException("'from' not less than to (field '$i')");
+            }
+        }
+    }
+
+    /**
+     * Access the fields definition.
+     *
+     * @return array the definition of the fields (start and end) to be read.
+     */
+    public function getFields()
+    {
+        return $this->fields;
+    }
+
+
+    /**
+     * Read and return the next line from the file.
+     *
+     * @param int $mode (SKIP_EMPTY_LINES or RETURN_EVERY_LINE which is the default)
+     * @return array|null the data from next line of the file or null if there are no more lines.
+     */
+    public function getLine($mode = self::RETURN_EVERY_LINE)
+    {
+        $line = parent::getLine($mode);
+        if ($line === null) { return null; }
+        return $this->splitLine($line);
+    }
+
+    /**
+     * Split a line of data (a string) into an array based on the $fields configuration array.
+     *
+     * @param string $line the line of "fixed width" data
+     * @return array
+     */
+    protected function splitLine($line)
+    {
+        $result = [];
+        foreach($this->fields as $field) {
+            $result[] = mb_substr($line, $field['from'], $field['to'] - $field['from']);
+        }
+        return $result;
+    }
+
+}
---
+++ tests/Fiedsch/Data/File/FixedWidthReaderTest.php
@@ -0,0 +1,112 @@
+<?php
+use Fiedsch\Data\File\Reader;
+use Fiedsch\Data\File\FixedWidthReader;
+use PHPUnit\Framework\TestCase;
+use PHPUnit\Framework\Assert;
+
+class FixedWidthReaderTest extends TestCase
+{
+    /**
+     * @var string
+     */
+    protected $filepath = 'tests/assets/data.fixed';
+
+    /**
+     * @var fields
+     */
+    protected $fields = [
+        ['from'=>0,'to'=>5],
+        ['from'=>5,'to'=>13],
+        ['from'=>13,'to'=>100],
+    ];
+
+
+    /**
+     * @var FixedWidthReader
+     */
+    protected $reader;
+
+    /**
+     * setup for all tests
+     */
+    protected function setUp()
+    {
+        $this->reader = new FixedWidthReader($this->filepath, $this->fields);
+    }
+
+    /**
+     * clean up after all tests
+     */
+    protected function tearDown()
+    {
+        $this->reader->close();
+    }
+
+    public function testGetLines()
+    {
+        $i = 0;
+        while (($data = $this->reader->getLine(FixedWidthReader::SKIP_EMPTY_LINES)) !== null) {
+            if ($i++ == 0) {
+                Assert::assertEquals('01234',    $data[0]);
+                Assert::assertEquals('56789012', $data[1]);
+                Assert::assertEquals('345',       $data[2]);
+            }
+        }
+        Assert::assertEquals(5, $i);
+        Assert::assertEquals(6, $this->reader->getLineNumber());
+    }
+
+    public function testShortLinesLines()
+    {
+        $lastLine = null;
+        while (($data = $this->reader->getLine(FixedWidthReader::SKIP_EMPTY_LINES)) !== null) {
+            $lastLine = $data;
+        }
+        var_dump($lastLine);
+        Assert::assertEquals(['last','',''], $lastLine);
+    }
+
+    public function testInvalidConfigNull()
+    {
+        $this->expectException(\TypeError::class);
+        $this->reader = new FixedWidthReader($this->filepath, null);
+    }
+
+    public function testInvalidConfigEmpty()
+    {
+        $this->expectException(\RuntimeException::class);
+        $this->reader = new FixedWidthReader($this->filepath, []);
+    }
+
+    public function testInvalidConfigMissingTo()
+    {
+        $this->expectException(\RuntimeException::class);
+        $this->reader = new FixedWidthReader($this->filepath, ['from'=>1]);
+    }
+
+    public function testInvalidConfigNegativeFrom()
+    {
+        $this->expectException(\RuntimeException::class);
+        $this->reader = new FixedWidthReader($this->filepath, ['from'=>-1]);
+    }
+
+    public function testInvalidConfigNegativeTo()
+    {
+        $this->expectException(\RuntimeException::class);
+        $this->reader = new FixedWidthReader($this->filepath, ['from'=>1, 'to'=>-1]);
+    }
+
+    public function testInvalidConfigToEqFrom()
+    {
+        $this->expectException(\RuntimeException::class);
+        $this->reader = new FixedWidthReader($this->filepath, ['from'=>3, 'to'=>3]);
+    }
+
+    public function testInvalidConfigToNotGtFrom()
+    {
+        $this->expectException(\RuntimeException::class);
+        $this->reader = new FixedWidthReader($this->filepath, ['from'=>3, 'to'=>-2]);
+    }
+
+}
+
